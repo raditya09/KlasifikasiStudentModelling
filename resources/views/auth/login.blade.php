9882baca0eb1f078d8e4b17f815ae5c336b66c5b
raditya09 <radityaariefp@polije.ac.id>
2023-08-25 23:07:17 +0700

    Update landing page, login dan register. Add Admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,11 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="col-lg-6">
+        <div class="row">
+        <!-- <div class="col-md-8"> -->
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <!-- <div class="card-header">{{ __('Masuk') }}</div> -->
 
                 <div class="card-body">
+
+                    <h3 class="fw-bolder">Selamat datang!</h3><br>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -40,34 +44,103 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-lg-4 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Ingat Saya') }}
                                     </label>
                                 </div>
+                            </div>
+                            <div class="col-lg-4">
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Lupa Kata Sandi?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                        </div>
+
+                        <div class="row mb-2">
+                            <div class="col-md-2 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Masuk') }}
+                                </button> 
                             </div>
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                            <div class="col-lg-8 offset-md-4">
+                                Belum mempunyai akun? <a class="btn btn-link" href="{{ route('register') }}">
+                                    {{ __('Daftar') }}
+                                </a> 
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+    </div>
+    <!-- Right side columns -->
+    <div class="col-lg-6">
+        <div class="card-body">
+            <h5 class="card-title">Recent Activity <span>| Today</span></h5>
+
+            <div class="activity">
+
+                <div class="activity-item d-flex">
+                <div class="activite-label">32 min</div>
+                <i class='bi bi-circle-fill activity-badge text-success align-self-start'></i>
+                <div class="activity-content">
+                    Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae
+                </div>
+                </div><!-- End activity item-->
+
+                <div class="activity-item d-flex">
+                <div class="activite-label">56 min</div>
+                <i class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
+                <div class="activity-content">
+                    Voluptatem blanditiis blanditiis eveniet
+                </div>
+                </div><!-- End activity item-->
+
+                <div class="activity-item d-flex">
+                <div class="activite-label">2 hrs</div>
+                <i class='bi bi-circle-fill activity-badge text-primary align-self-start'></i>
+                <div class="activity-content">
+                    Voluptates corrupti molestias voluptatem
+                </div>
+                </div><!-- End activity item-->
+
+                <div class="activity-item d-flex">
+                <div class="activite-label">1 day</div>
+                <i class='bi bi-circle-fill activity-badge text-info align-self-start'></i>
+                <div class="activity-content">
+                    Tempore autem saepe <a href="#" class="fw-bold text-dark">occaecati voluptatem</a> tempore
+                </div>
+                </div><!-- End activity item-->
+
+                <div class="activity-item d-flex">
+                <div class="activite-label">2 days</div>
+                <i class='bi bi-circle-fill activity-badge text-warning align-self-start'></i>
+                <div class="activity-content">
+                    Est sit eum reiciendis exercitationem
+                </div>
+                </div><!-- End activity item-->
+
+                <div class="activity-item d-flex">
+                <div class="activite-label">4 weeks</div>
+                <i class='bi bi-circle-fill activity-badge text-muted align-self-start'></i>
+                <div class="activity-content">
+                    Dicta dolorem harum nulla eius. Ut quidem quidem sit quas
+                </div>
+                </div><!-- End activity item-->
+
+            </div>
+
+            </div>
+        </div><!-- End Recent Activity -->
     </div>
 </div>
 @endsection
