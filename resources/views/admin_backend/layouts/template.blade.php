df5ef201b256574d6e5a0caa0176e5d2b80a567c
raditya09 <radityaariefp@polije.ac.id>
2023-10-27 12:51:24 +0700

    update landingpage

--- resources/views/admin_backend/layouts/template.blade.php
+++ resources/views/admin_backend/layouts/template.blade.php
@@ -1,224 +1,221 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>{{config('app.name')}}</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <title>{{config('app.name')}}</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <!-- Favicons -->
+    <link href="{{ asset('admin_backend/assets/img/favicon.png')}}" rel="icon">
+    <link href="{{ asset('admin_backend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
-  <!-- Favicons -->
-  <link href="{{ asset('admin_backend/assets/img/favicon.png')}}" rel="icon">
-  <link href="{{ asset('admin_backend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('admin_backend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('admin_backend/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{ asset('admin_backend/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('admin_backend/assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
+    <link href="{{ asset('admin_backend/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
+    <link href="{{ asset('admin_backend/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{ asset('admin_backend/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('admin_backend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('admin_backend/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{ asset('admin_backend/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('admin_backend/assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
-  <link href="{{ asset('admin_backend/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
-  <link href="{{ asset('admin_backend/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-  <link href="{{ asset('admin_backend/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('admin_backend/assets/css/style.css')}}" rel="stylesheet">
+  <!-- 
+    =======================================================
+    * Template Name: NiceAdmin
+    * Updated: May 30 2023 with Bootstrap v5.3.0
+    * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+  </head>
 
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('admin_backend/assets/css/style.css')}}" rel="stylesheet">
-<!-- 
- 
+  <body>
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Updated: May 30 2023 with Bootstrap v5.3.0
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top d-flex align-items-center">
 
-<body>
+      <div class="d-flex align-items-center justify-content-between">
+        <a href="#" class="logo d-flex align-items-center">
+          <img src="{{ asset('admin_backend/assets/img/logo.png')}}" alt="">
+          <span class="d-none d-lg-block">{{config('app.name')}}</span>
+        </a>
+        <i class="bi bi-list toggle-sidebar-btn"></i>
+      </div><!-- End Logo -->
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top d-flex align-items-center">
+      <nav class="header-nav ms-auto">
+        <ul class="d-flex align-items-center">
 
-    <div class="d-flex align-items-center justify-content-between">
-      <a href="#" class="logo d-flex align-items-center">
-        <img src="{{ asset('admin_backend/assets/img/logo.png')}}" alt="">
-        <span class="d-none d-lg-block">{{config('app.name')}}</span>
-      </a>
-      <i class="bi bi-list toggle-sidebar-btn"></i>
-    </div><!-- End Logo -->
+          <li class="nav-item dropdown">
 
-    <nav class="header-nav ms-auto">
-      <ul class="d-flex align-items-center">
+            <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+              <i class="bi bi-bell"></i>
+              <span class="badge bg-primary badge-number">4</span>
+            </a><!-- End Notification Icon -->
 
-        <li class="nav-item dropdown">
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+              <li class="dropdown-header">
+                You have 4 new notifications
+                <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
 
-          <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-            <i class="bi bi-bell"></i>
-            <span class="badge bg-primary badge-number">4</span>
-          </a><!-- End Notification Icon -->
+              <li class="notification-item">
+                <i class="bi bi-exclamation-circle text-warning"></i>
+                <div>
+                  <h4>Lorem Ipsum</h4>
+                  <p>Quae dolorem earum veritatis oditseno</p>
+                  <p>30 min. ago</p>
+                </div>
+              </li>
 
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-            <li class="dropdown-header">
-              You have 4 new notifications
-              <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
 
-            <li class="notification-item">
-              <i class="bi bi-exclamation-circle text-warning"></i>
-              <div>
-                <h4>Lorem Ipsum</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>30 min. ago</p>
-              </div>
-            </li>
+              <li class="notification-item">
+                <i class="bi bi-x-circle text-danger"></i>
+                <div>
+                  <h4>Atque rerum nesciunt</h4>
+                  <p>Quae dolorem earum veritatis oditseno</p>
+                  <p>1 hr. ago</p>
+                </div>
+              </li>
 
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
 
-            <li class="notification-item">
-              <i class="bi bi-x-circle text-danger"></i>
-              <div>
-                <h4>Atque rerum nesciunt</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>1 hr. ago</p>
-              </div>
-            </li>
+              <li class="notification-item">
+                <i class="bi bi-check-circle text-success"></i>
+                <div>
+                  <h4>Sit rerum fuga</h4>
+                  <p>Quae dolorem earum veritatis oditseno</p>
+                  <p>2 hrs. ago</p>
+                </div>
+              </li>
 
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
 
-            <li class="notification-item">
-              <i class="bi bi-check-circle text-success"></i>
-              <div>
-                <h4>Sit rerum fuga</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>2 hrs. ago</p>
-              </div>
-            </li>
+              <li class="notification-item">
+                <i class="bi bi-info-circle text-primary"></i>
+                <div>
+                  <h4>Dicta reprehenderit</h4>
+                  <p>Quae dolorem earum veritatis oditseno</p>
+                  <p>4 hrs. ago</p>
+                </div>
+              </li>
 
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li class="dropdown-footer">
+                <a href="#">Show all notifications</a>
+              </li>
 
-            <li class="notification-item">
-              <i class="bi bi-info-circle text-primary"></i>
-              <div>
-                <h4>Dicta reprehenderit</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>4 hrs. ago</p>
-              </div>
-            </li>
+            </ul><!-- End Notification Dropdown Items -->
 
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li class="dropdown-footer">
-              <a href="#">Show all notifications</a>
-            </li>
+          </li><!-- End Notification Nav -->
+        </ul>
+      </nav><!-- End Icons Navigation -->
 
-          </ul><!-- End Notification Dropdown Items -->
+    </header><!-- End Header -->
 
-        </li><!-- End Notification Nav -->
-      </ul>
-    </nav><!-- End Icons Navigation -->
+    @include('admin_backend/layouts.sidebar')
 
-  </header><!-- End Header -->
+    @yield('content')
+    
+    @if (session('status'))
+      <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+      </div>
+    @endif
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+      <div class="copyright">
+        &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </footer><!-- End Footer -->
 
-  @include('admin_backend/layouts.sidebar')
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  @yield('content')
-  
-  @if (session('status'))
-    <div class="alert alert-success" role="alert">
-      {{ session('status') }}
-    </div>
-  @endif
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
-    <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-    </div>
-  </footer><!-- End Footer -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('js/jquery.js')}}"></script>
-  <script src="{{ asset('admin_backend/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
-  <script src="{{ asset('admin_backend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ asset('admin_backend/assets/vendor/chart.js/chart.umd.js')}}"></script>
-  <script src="{{ asset('admin_backend/assets/vendor/echarts/echarts.min.js')}}"></script>
-  <script src="{{ asset('admin_backend/assets/vendor/quill/quill.min.js')}}"></script>
-  <script src="{{ asset('admin_backend/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
-  <script src="{{ asset('admin_backend/assets/vendor/tinymce/tinymce.min.js')}}"></script>
-  <script src="{{ asset('admin_backend/assets/vendor/php-email-form/validate.js')}}"></script>
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('js/jquery.js')}}"></script>
+    <script src="{{ asset('admin_backend/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+    <script src="{{ asset('admin_backend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('admin_backend/assets/vendor/chart.js/chart.umd.js')}}"></script>
+    <script src="{{ asset('admin_backend/assets/vendor/echarts/echarts.min.js')}}"></script>
+    <script src="{{ asset('admin_backend/assets/vendor/quill/quill.min.js')}}"></script>
+    <script src="{{ asset('admin_backend/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
+    <script src="{{ asset('admin_backend/assets/vendor/tinymce/tinymce.min.js')}}"></script>
+    <script src="{{ asset('admin_backend/assets/vendor/php-email-form/validate.js')}}"></script>
 
 
-  <!-- Template Main JS File -->
-  <script src="{{ asset('admin_backend/assets/js/main.js')}}"></script>
-  <script src="{{ asset('css/sweealert2.all.min.js')}}"></script>
+    <!-- Template Main JS File -->
+    <script src="{{ asset('admin_backend/assets/js/main.js')}}"></script>
+    <script src="{{ asset('css/sweealert2.all.min.js')}}"></script>
 
-  @yield('script')
+    @yield('script')
 
-  @if (session('success'))
-    <script>
-      const Toast = Swal.mixin({
-          toast: true,
-          position: 'top-end',
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-          }
+    @if (session('success'))
+      <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          });
+
+          Toast.fire({
+            icon: 'success',
+            title: '{{ session('success') }}'
         });
+      </script>
+    @endif
 
-        Toast.fire({
-          icon: 'success',
-          title: '{{ session('success') }}'
-      });
-    </script>
-  @endif
+    @if (session('error'))
+      <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          });
 
-  @if (session('error'))
-    <script>
-      const Toast = Swal.mixin({
-          toast: true,
-          position: 'top-end',
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-          }
+          Toast.fire({
+            icon: 'error',
+            title: '{{ session('error') }}'
         });
-
-        Toast.fire({
-          icon: 'error',
-          title: '{{ session('error') }}'
-      });
-    </script>
-  @endif
-</body>
+      </script>
+    @endif
+  </body>
 
 </html>
