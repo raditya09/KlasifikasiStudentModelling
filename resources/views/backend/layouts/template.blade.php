4ea63da51f20e432ced89392780598372f319325
Ahmad Firdaus Tarmidzi <e41211991@student.polije.ac.id>
2023-11-08 13:57:09 +0700

    up

--- resources/views/backend/layouts/template.blade.php
+++ resources/views/backend/layouts/template.blade.php
@@ -10,8 +10,8 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('backend/assets/img/favicon.png')}}" rel="icon">
-  <link href="{{ asset('backend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+  <link href="{{ asset('backend/assets/img/logoPolije.png')}}" rel="icon">
+  <link href="{{ asset('backend/assets/img/logoPolije.png')}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
@@ -45,7 +45,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="#" class="logo d-flex align-items-center">
-        <img src="{{ asset('backend/assets/img/logo.png')}}" alt="">
+        <img src="{{ asset('backend/assets/img/logoPolije.png')}}" alt="">
         <span class="d-none d-lg-block">{{config('app.name')}}</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
